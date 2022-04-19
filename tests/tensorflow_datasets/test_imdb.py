f75fd6787c299c3837672126482786accc562bdc
CookieBox26 <34176970+CookieBox26@users.noreply.github.com>
2022-04-19 21:35:00 +0900

    add tests/tensorflow_datasets/test_imdb.py

---
+++ tests/tensorflow_datasets/test_imdb.py
@@ -0,0 +1,21 @@
+import tensorflow_datasets as tfds
+
+
+class TestImdb:
+
+    def test(self):
+        data = tfds.load('imdb_reviews')
+        assert len(data['train']) == 25000
+        assert len(data['test']) == 25000
+
+        data_train = data['train'].take(5)
+
+        vocabulary = set()
+        tokenizer = tfds.deprecated.text.Tokenizer()
+        for x in data_train:
+            vocabulary.update(tokenizer.tokenize(x['text'].numpy()))
+
+        encoder = tfds.deprecated.text.TokenTextEncoder(vocabulary)
+        for x in data_train:
+            encoded = encoder.encode(x['text'].numpy())
+            assert type(encoded) == list  # 単語インデックスのリスト
