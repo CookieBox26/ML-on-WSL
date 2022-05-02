238f70d7897d608bf649951894105043be863aa9
CookieBox26 <34176970+CookieBox26@users.noreply.github.com>
2022-05-02 23:56:45 +0900

    update

--- tests/flax/test_linen.py
+++ tests/flax/test_linen.py
@@ -1,21 +1,24 @@
 from typing import Sequence
 import jax
 import jax.numpy as jnp
-import flax.linen as nn
+from flax import linen
 
 
-class MLP(nn.Module):
+class MLP(linen.Module):
     features: Sequence[int]
 
-    @nn.compact
+    @linen.compact
     def __call__(self, x):
         for feat in self.features[:-1]:
-            x = nn.relu(nn.Dense(feat)(x))
-        x = nn.Dense(self.features[-1])(x)
+            x = linen.relu(linen.Dense(feat)(x))
+        x = linen.Dense(self.features[-1])(x)
         return x
 
 
 class TestFlaxLinen:
+    def test(self):
+        key = jax.random.PRNGKey(26)
+
     def test_mlp(self):
         model = MLP([12, 8, 4])
         batch = jnp.ones((32, 10))
